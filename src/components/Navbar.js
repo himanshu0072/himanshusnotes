bdc5aa40536dabf11f6f44d5c1e6d64b9a707536
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-02 01:31:46 +0530

    addig favicon logo and some refacotoring in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,8 +43,8 @@ export default function Navbar() {
                   <a className="nav-link" href="/search">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
+                      width="25"
+                      height="25"
                       fill="currentColor"
                       class="bi bi-search"
                       viewBox="0 0 16 16">
@@ -54,19 +54,10 @@ export default function Navbar() {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/more">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-distribute-vertical"
-                    viewBox="0 0 16 16">
-                    <path
-                      fill-rule="evenodd"
-                      d="M1 1.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 0-1h-13a.5.5 0 0 0-.5.5zm0 13a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 0-1h-13a.5.5 0 0 0-.5.5z"/>
-                    <path d="M2 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7z" />
-                  </svg>
-                  </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                 </a>
                 </li>
               </ul>
             </div>
