190edf460953e7a301a426c03610133ce0de6185
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-02 22:14:43 +0530

    SubNews Coponent Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MainBlog from './components/MainBlog';
 import Navbar from './components/Navbar';
+import SubNews from './components/SubNews';
 
 function App() {
   return (
     <>
     <Navbar/>
     <MainBlog/>
+    <SubNews/>
     </>
   );
 }
