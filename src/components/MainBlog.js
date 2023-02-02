c8c01f42e242722d3986e326e7973d47368906cf
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-02 21:08:07 +0530

    MainBlogcomponent is ready

--- src/components/MainBlog.js
+++ src/components/MainBlog.js
@@ -4,14 +4,13 @@ export default function MainBlog() {
   return (
     <>
     <div className="MainNews">
-    
         <div className="leftDiv">
             <img src="https://media.gatesnotes.com/-/media/Images/Articles/Energy/African-Smallholder-Farmers-Adaptation-and-Innovation/African_Smallholder_farmers_20230104_homepage-hero_568x408.ashx" alt="MAin News"/>  
         </div>
         <div className="rightDiv">
-            <p className="heromodule_label">Farm work</p>
-            <p className="heromodule_title" >On Africa’s farms, the forecast calls for adaptation and innovation.</p>
-            <p className="heromodule_description">In Kenya, I visited with a smallholder farmer using new tools and practices to fight back against climate change.</p>
+            <p className="NewsTitle"><u>Farm work</u></p>
+            <p className="NewsHeading" >On Africa’s farms, the forecast calls for adaptation and innovation.</p>
+            <p className="NewDesc">In Kenya, I visited with a smallholder farmer using new tools and practices to fight back against climate change.</p>
             <small class="text-muted">By Bill Gates | January 31, 2023 </small>
         </div>
     </div>
