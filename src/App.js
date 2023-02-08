e03c9dc686f2f3585a71dc7ca1f0ca961c9cd0f6
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-08 22:54:25 +0530

    Adding andd repeating Components in index.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
-import Education from './components/Education';
-import Footer from './components/Footer';
-import LatestVideo from './components/LatestVideo';
-import MainBlog from './components/MainBlog';
-import Navbar from './components/Navbar';
-import RecentBooks from './components/RecentBooks';
-import SubNews from './components/SubNews';
+import "./App.css";
+import Education from "./components/Education";
+import Footer from "./components/Footer";
+import LatestVideo from "./components/LatestVideo";
+import MainBlog from "./components/MainBlog";
+import Navbar from "./components/Navbar";
+import RecentBooks from "./components/RecentBooks";
+import SubNews from "./components/SubNews";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <MainBlog/>
-    <SubNews/>
-    <LatestVideo/>
-    <Education/>
-    <RecentBooks/>
-    <Footer/>
+      <Navbar />
+      <MainBlog />
+      <SubNews />
+      <LatestVideo />
+      <Education />
+      <RecentBooks />
+      <Education />
+      <Footer />
     </>
   );
 }
